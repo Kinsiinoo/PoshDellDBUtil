b6eec7dd6457dc41029e5e35c9839994239ddb9a
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-05-08 17:50:27 +0200

    Added parameters

--- PoshDellDBUtil.psm1
+++ PoshDellDBUtil.psm1
@@ -1,3 +1,11 @@
 ﻿function Invoke-PoshDDClean {
-    
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Computers,
+        [Parameter(Position=1)]
+        [ValidateSet("Scan", "Scan&Clean")]
+        [String]$PoshDDMode = "Scan"
+    )
 }
