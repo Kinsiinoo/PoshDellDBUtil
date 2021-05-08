9f772e95b5c47c5390c48870cf8bea3ea2d2d601
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-05-08 17:55:45 +0200

    Get computers from OU and test connection

--- PoshDellDBUtil.psm1
+++ PoshDellDBUtil.psm1
@@ -3,9 +3,20 @@
     param (
         [Parameter(Mandatory=$true,Position=0)]
         [ValidateNotNullOrEmpty()]
-        [String]$Computers,
+        [String]$ComputerOU,
         [Parameter(Position=1)]
         [ValidateSet("Scan", "Scan&Clean")]
         [String]$PoshDDMode = "Scan"
     )
+
+    $Computers = Get-ADComputer -Filter * -SearchBase "$($ComputerOU)" | Select-Object -Property Name | Sort-Object -Property Name
+
+    foreach ($Computer in $Computers) {
+        if (Test-Connection -ComputerName $Computer.Name -Quiet -Count 2 -ErrorAction SilentlyContinue) {
+
+        } else {
+
+        }
+    }
+
 }
