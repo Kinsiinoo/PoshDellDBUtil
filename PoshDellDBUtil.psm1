1be967dca539712418a0b57bbbc4d36f4b32e498
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-05-08 18:08:32 +0200

    Scan for DBUtil in Windows temp folder

--- PoshDellDBUtil.psm1
+++ PoshDellDBUtil.psm1
@@ -4,18 +4,24 @@
         [Parameter(Mandatory=$true,Position=0)]
         [ValidateNotNullOrEmpty()]
         [String]$ComputerOU,
-        [Parameter(Position=1)]
-        [ValidateSet("Scan", "Scan&Clean")]
-        [String]$PoshDDMode = "Scan"
+        [Parameter(Mandatory=$false,Position=1)]
+        [Switch]$Clean
     )
 
     $Computers = Get-ADComputer -Filter * -SearchBase "$($ComputerOU)" | Select-Object -Property Name | Sort-Object -Property Name
 
     foreach ($Computer in $Computers) {
         if (Test-Connection -ComputerName $Computer.Name -Quiet -Count 2 -ErrorAction SilentlyContinue) {
-
+            Write-Host "$($Computer.Name):" -ForegroundColor White -BackgroundColor DarkGreen
+            Invoke-Command -ComputerName $Computer.Name -ErrorAction SilentlyContinue -ScriptBlock {
+                if(Test-Path -Path C:\Windows\Temp\DBUtil_2_3.Sys -PathType Leaf){
+                    Write-Host "Windows->Temp folder: DBUtil_2_3.Sys found!" -ForegroundColor White -BackgroundColor DarkRed
+                } else {
+                    Write-Host "DBUtil_2_3.Sys not found!" -ForegroundColor White -BackgroundColor DarkGreen
+                }
+            }
         } else {
-
+            Write-Host "$($Computer.Name) not reachable!" -ForegroundColor White -BackgroundColor DarkRed
         }
     }
 
